2750e8cf886b2c037db9c0434d8985c7ce0818be
yunhozz <qkrdbsgh1121@naver.com>
2024-10-07 19:56:36 +0900

    [#1] UPDATE: Auth 모듈에 config 및 데이터베이스 관련 모듈 import

--- apps/auth/src/auth.module.ts
+++ apps/auth/src/auth.module.ts
@@ -1,10 +1,22 @@
 import { Module } from '@nestjs/common';
 import { AuthController } from './auth.controller';
 import { AuthService } from './auth.service';
+import { ConfigModule } from '@nestjs/config';
+import { DatabaseModule } from '@app/common';
+import Joi from 'joi';
 
 @Module({
-  imports: [],
-  controllers: [AuthController],
-  providers: [AuthService],
+    imports: [
+        ConfigModule.forRoot({
+            isGlobal: true,
+            validationSchema: Joi.object({
+                MONGODB_URI: Joi.string().required()
+            }),
+            envFilePath: "./apps/auth/.env"
+        }),
+        DatabaseModule
+    ],
+    controllers: [AuthController],
+    providers: [AuthService],
 })
 export class AuthModule {}
